99d00e5d35680447122d99538cd63a3efcc2c093
Jdcamacaro <davidjcamacaro@gmail.com>
2024-02-12 15:16:55 -0400

    commit

--- src/main/java/com/clienteFactura/controllers/ControllerCliente.java
+++ src/main/java/com/clienteFactura/controllers/ControllerCliente.java
@@ -1,5 +1,23 @@
 package com.clienteFactura.controllers;
 
-public class ControllerCliente {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.clienteFactura.services.ClienteService;
+
+    @RestController
+    @RequestMapping
+
+public class ControllerCliente {    
+
+    @Autowired
+    private ClienteService clienteService;
+
+
+    @GetMapping
+    
+
 
 }
--- src/main/java/com/clienteFactura/controllers/FacturaController.java
+++ src/main/java/com/clienteFactura/controllers/FacturaController.java
@@ -1,5 +1,17 @@
 package com.clienteFactura.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.clienteFactura.services.FacturaServices;
+
+@RestController
+@RequestMapping
 public class FacturaController {
 
+    @Autowired
+    private FacturaServices facturaServices;
+
+    
 }
